0c72457c574b02793551cce543d9dc2a1811949b
ArtemP31 <pysariuk2022tl31@gmail.com>
2024-09-17 16:30:18 +0300

    dff

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,9 +1,6 @@
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let gall = new SimpleLightbox('.gallery a', { 
-
-});
 
 
 console.log(SimpleLightbox);
